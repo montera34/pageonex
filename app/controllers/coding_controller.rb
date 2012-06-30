2f04259fd86a7082aec8730bc90f323e38f4aadf
ahmd refat <ahmd.refat.twfik@gmail.com>
2012-06-30 02:55:35 +0200

    coding controller

---
+++ app/controllers/coding_controller.rb
@@ -0,0 +1,11 @@
+class CodingController < ApplicationController
+	before_filter :authenticate_user!
+
+	def get_images
+		
+	end
+
+	def process_images
+		
+	end
+end
---
+++ spec/controllers/coding_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CodingController do
+
+end
