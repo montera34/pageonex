aeadd640c5fd30e24526a1d6fa963c8705045210
ahmd refat <ahmd.refat.twfik@gmail.com>
2012-07-14 05:24:35 +0200

    refactor part of the code to fit the new view

--- app/controllers/coding_controller.rb
+++ app/controllers/coding_controller.rb
@@ -13,23 +13,58 @@ class CodingController < ApplicationController
 			end
 		end
 		
-		# render json: @highlighted_areas.to_json
 	end
 
-	def display
+	def process_highlighted_areas
 		@thread = Threadx.find_by_thread_name params[:thread_name]	
 		@images = @thread.images
 		@image_counter = @thread.images.length
-		@image_counter.downto(1) do |c|
-				2.downto(1) do |hc|
-					if params["image#{c}_ha#{hc}"] == "1"
-						
-						highlighted_area = HighlightedArea.create!({:image => @images[c-1], user: current_user, code_id: params["image#{c}_ha#{hc}_code_id"].to_i})
 
-						Area.create!({x1: params["image#{c}_ha#{hc}_x1"].to_i, y1: params["image#{c}_ha#{hc}_y1"].to_i, x2: params["image#{c}_ha#{hc}_x2"].to_i, y2: params["image#{c}_ha#{hc}_y2"].to_i, width: params["image#{c}_ha#{hc}_width"].to_i, height: params["image#{c}_ha#{hc}_height"].to_i, highlighted_area: highlighted_area})
-					end
-				end	
+		# check for the number of highlighted areas, if it more than 0 that's mean, the user trying to modify, existing highlighted areas
+		no_highlighted_area = 0
+		@image_counter.downto(1) do |c|
+			2.downto(1) do |hc|
+				if params["image#{c}_ha#{hc}"] == "1"
+					no_highlighted_area += params["image#{c}_ha#{hc}"].to_i
+				end
+			end
 		end
+
+		if (no_highlighted_area != 0)
+			@highlighted_areas = []
+			@thread.codes.each do |code|
+				code.highlighted_areas.each do |high_area|
+					high_area.areas[0].destroy
+					high_area.destroy
+				end
+			end
+		end
+
+		if (@thread.status == "opened")
+				
+				@image_counter.downto(1) do |c|
+					2.downto(1) do |hc|
+						if params["image#{c}_ha#{hc}"] == "1"
+							
+							highlighted_area = HighlightedArea.create!({:name => "image#{c}_ha#{hc}" ,:image => @images[c-1], user: current_user, code_id: params["image#{c}_ha#{hc}_code_id"].to_i})
+
+							Area.create!({x1: params["image#{c}_ha#{hc}_x1"].to_i, y1: params["image#{c}_ha#{hc}_y1"].to_i, x2: params["image#{c}_ha#{hc}_x2"].to_i, y2: params["image#{c}_ha#{hc}_y2"].to_i, width: params["image#{c}_ha#{hc}_width"].to_i, height: params["image#{c}_ha#{hc}_height"].to_i, highlighted_area: highlighted_area})
+						end
+					end	# ends of [2.downto(1)]
+					
+				end # ends of [@image_counter.downto(1)] block
+		else
+			# notifiy the user that he cannot modifiy highlighted areas
+		end
+
+		redirect_to "/threads/#{@thread.thread_name}/display"
+	end
+
+	def display
+		@thread = Threadx.find_by_thread_name params[:thread_name]		
+		@images = @thread.images
+		@image_counter = @thread.images.length
+		@highlighted_areas = []
 
 		@highlighted_areas = []
 		@thread.codes.each do |code|
@@ -38,6 +73,5 @@ class CodingController < ApplicationController
 			end
 		end
 
-		# render json: params.to_json
 	end
 end
--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -10,7 +10,13 @@ class ThreadsController < ApplicationController
 		@thread.update_attributes!(params[:threadx])
 		@thread.thread_name = params[:threadx]["thread_display_name"].sub(' ', '_')
 
+
+		# this array is made to passed to Scraper.get_issues method, because this method accept the specific format of newspapers names as the following
+		# {"es" => ["elpais", "abc"], "de" => ["faz", "bild"], "fr" => ["lemonde", "lacroix"], "it" => ["corriere_della_sera", "ilmessaggero"], "uk" => ["the_times", ],"us" => ["wsj", "newyork_times", "usa_today"]}
+		# city attribute holds the country code like {"es", "de", ...}
+		# name attribute holds the name of the newspaper {"elpais", "abc", ...}
 		newspapers_names = {}
+
 		number_of_newspapers = params[:media_count].to_i
 		number_of_newspapers.downto(1) do |n|
 			_media = Media.find(params["media#{n}"])
@@ -24,16 +30,20 @@ class ThreadsController < ApplicationController
 
 		end
 
+		# create object for each code submited
 		number_of_topics = params[:topic_count].to_i
 		number_of_topics.downto(1) do |n|
 			Code.create!({:code_text => params["topic_name_#{n}"], :code_description => params["topic_description_#{n}"],:color => params["topic_color_#{n}"],:threadx_id => @thread.id})
 		end
 
+		# array of object refers to scraped images
 		images = []
 
+		# passes dates and formated newspapers_names array, and the return is a hash contains info about each images(publication_date, media, local_path)
 		newspapers_images = Scraper.get_issues(@thread.start_date.year,@thread.start_date.month,@thread.start_date.day,@thread.end_date.day, newspapers_names)
 
 		newspapers_images.each do |image_name, image_info|
+			# search if the image dose not exsit, it create an object for this image 
 			if ( (image = Image.find_by_image_name image_name) == nil)
 				image_info["image_name"] = image_name
 				media = Media.find_by_name(image_info[:media])
@@ -41,16 +51,18 @@ class ThreadsController < ApplicationController
 				image = Image.create!({ image_name: image_info["image_name"],publication_date: image_info[:publication_date], local_path: image_info[:local_path], media_id: media.id})
 				
 				images << image
+			# otherwise it find the image, and add to the array
 			else
 				image = Image.find_by_image_name(image_name)
 				images << image
 			end
 		end
 
+		# add reference to the scraped images to the thread
 		@thread.images << images
+
+		# specifying the owner, because there is a collaborators (related to Intercoder)
 		@thread.owner_id = current_user.id
-		# @thread.status = "open"
-		# @thread.category = "uncategorized"
 		@thread.save!
 
 		redirect_to "/threads/#{@thread.thread_name}/coding"
@@ -71,7 +83,6 @@ class ThreadsController < ApplicationController
 	end
 
 	def update
-		
 		@thread = Threadx.find_by_thread_name params[:id]
 		@thread.update_attributes(params[:threadx])
 		@thread.thread_name = params[:threadx]["thread_display_name"].sub(' ', '_')
@@ -80,7 +91,8 @@ class ThreadsController < ApplicationController
 	end
 
 	def show
-		render :text => params[:id]
+		@thread = Threadx.find_by_thread_name params[:id]
+		redirect_to "/threads/#{@thread.thread_name}/display"
 	end
 
 end
