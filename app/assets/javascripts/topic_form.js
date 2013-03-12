3a64df92f278656eeb92811404b7d828d588efe3
Edward L Platt <ed@elplatt.com>
2013-03-12 17:27:57 -0400

    Adds UI for deleting topic.

---
+++ app/assets/javascripts/topic_form.js
@@ -0,0 +1,7 @@
+$(function () {
+  // Delete a topic
+  $('.delete_topic').click(function() {
+    var index = $(this).attr('id').substr('delete_topic_'.length);
+    $("#topic_deleted_" + index).val('1');
+  });
+});
