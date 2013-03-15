317f9e35dbf71fe1ecdbbc5120462542d361604f
rporres <rporres@gmail.com>
2013-03-15 14:28:10 +0100

    Included local newspapers to newspaper list
    - parse_kiosko.pl now fetches more newspapers and sorts them
      to avoid duplicates
    - Updated kiosko.csv with the output of parse_kiosko.pl

--- public/parse_kiosko.pl
+++ public/parse_kiosko.pl
@@ -6,11 +6,9 @@ use utf8;
 use LWP::UserAgent;
 use HTML::TreeBuilder 5 -weak;
 use URI;
+use Data::Dump qw/pp/;
 
 use constant LANG => qw(en es fr);
-
-# No stdout buffering
-$|++;
 
 # Force UTF8 output
 binmode(STDOUT, ":utf8");
@@ -51,23 +49,15 @@ my @country_pages =
     grep { ! $uniq{$_}++ } 
         sort grep { ! m:/geo/: } map { $_->attr('href') } @a_tags;
 
-# Header
-print "Country,Country code,Newspaper name,Newspaper code,Newspaper url\n";
+#@country_pages = '/ar/';
+
+my @output = (
+    [qw(Country Country_code Newspaper_name Newspaper_code Newspaper_url)]
+);
 
 for my $country_page (@country_pages) {
     my $p_uri = URI->new_abs($country_page, $kiosko_url);
     my $p_tree = HTML::TreeBuilder->new_from_url($p_uri->as_string);
-
-    # Get title of this page to have the country
-    # Title of pages have the format i.e 
-    # "Periódicos de Argentina. Toda la prensa de hoy. Kiosko.net"
-    # "Periódicos de R. Dominicana. Toda la prensa de hoy. Kiosko.net"
-    my ($p_title_tag) = $p_tree->look_down('_tag', 'title');
-    
-    # Cannot do it this way due to Dominican Republic :(
-    #my ($country) = (split /\s/, (split /\./, $p_title_tag->as_text)[0])[-1];
-
-    my ($country) = ($p_title_tag->as_text =~ qr|$regexp{$lang}|) ? $1 : $p_title_tag->as_text;
 
     # We search through all the newspaper types in titPpal class
     my @tit_ppal = $p_tree->look_down(
@@ -77,20 +67,57 @@ for my $country_page (@country_pages) {
          }
     );
 
-    # TODO: Search also in all geo papers in the class auxCol. Then sort as we may have
+    push @output, get_thcover_data($p_uri, @tit_ppal);
+
+    # Search also in all geo papers in the class auxCol. Then sort as we may have
     # duplicates with those fetched in the titPpal class 
+    my $aux_col = $p_tree->look_down(
+        '_tag', 'div', sub {
+            defined $_[0]->attr('class') && 
+            $_[0]->attr('class') eq 'auxCol' 
+         }
+    );
 
-    for my $t (@tit_ppal) {
-        my $a_tit_ppal = $t->look_down('_tag', 'a', sub { defined $_[0]->attr('href') } );
+    my @li_aux_col = $aux_col->look_down(
+        '_tag', 'li', sub { 
+            defined $_[0]->attr('class') && 
+            $_[0]->attr('class') eq 'reg' 
+        }
+    );
 
-        defined $a_tit_ppal or next;
+    push @output, get_thcover_data($p_uri, @li_aux_col);
 
-        my $tit_ppal_uri = URI->new_abs($a_tit_ppal->attr('href'), $p_uri);
-        my $titppal_tree = HTML::TreeBuilder->new_from_url($tit_ppal_uri->as_string);
+}
+
+# sort @output to get unique members
+# we stringify the array refs as they will be printed to
+# compare entries.
+%uniq = ();
+my @output_sorted = 
+    grep { ! $uniq{$_}++ } 
+    map  { join ',', @$_ }
+    @output;
+
+print "$_\n" for @output_sorted;
+
+sub usage {
+    die sprintf "Usage: $0 <%s>\n", join '|', (LANG);
+}
+
+sub get_thcover_data {
+    my ($p_uri, @elements) = @_;
+
+    my @output;
+    for my $e (@elements) {
+        my $a_top = $e->look_down('_tag', 'a', sub { defined $_[0]->attr('href') } );
+        defined $a_top or next;
+        my $top_uri = URI->new_abs($a_top->attr('href'), $p_uri);
+
+        my $tree = HTML::TreeBuilder->new_from_url($top_uri);
 
         # Links of the newspapers are in the a tags having
         # class=thcover and the attribute href defined
-        my @titppal_a_tags = $titppal_tree->look_down(
+        my @a_tags = $tree->look_down(
             '_tag', 'a',
             sub {
                 defined $_[0]->attr('class') && 
@@ -99,7 +126,8 @@ for my $country_page (@country_pages) {
             }
         );
 
-        for my $a (@titppal_a_tags) {
+
+        for my $a (@a_tags) {
             my $n_uri = URI->new_abs($a->attr('href'), $kiosko_url);
             my $n_tree = HTML::TreeBuilder->new_from_url($n_uri->as_string);
 
@@ -108,17 +136,24 @@ for my $country_page (@country_pages) {
             # Get newspaper name
             # Newspaper pages names have the format, i.e
             # "Periódico Diario Libre (R. Dominicana). Periódicos de R. Dominicana. Toda la prensa de hoy. Kiosko.net"
-            my $n_name = $n_title_tag->as_text;
-            $n_name =~ s/ \(.+//;
-            $n_name =~ s/[^\s]+\s//;
+            my $n_name;
+            my $country;
+            if ($n_title_tag->as_text =~ /
+                (?:Periódico|Newspaper|Journal)\s
+                ( [^(]+ )\s
+                \( ( [^)]+ ) \) \.
+            /x) {
+                $n_name  = $1;
+                $country = $2;
+            }
+            else {
+                die "Cannot parse page ", $n_uri->as_string, " title";
+            }
 
             # Get newspaper code
             # newspaper links have the format: /fr/np/presseocean.html
-            my ($n_code) = ( split /\//, $a->attr('href') )[-1];
+            my (undef, $country_page, undef, $n_code) = split /\//, $a->attr('href');
             $n_code =~ s/\.html//;
-
-            # Remove slashes from country_page
-            $country_page =~ s:/::g;
 
             # Get newspaper url
             # Newspapers links are inside frontPageImage div classes,
@@ -157,11 +192,9 @@ for my $country_page (@country_pages) {
                 $newspaper_url = 'UNKNOWN';
             }
                 
-            print join(',', $country, $country_page, $n_name, $n_code, $newspaper_url), "\n";
+            push @output, [$country, $country_page, $n_name, $n_code, $newspaper_url];
         }
     }
-}
 
-sub usage {
-    die sprintf "Usage: $0 <%s>\n", join '|', (LANG);
+    return @output;
 }
