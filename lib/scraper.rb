8597b63d6e08938842ab3cdb865482ffb0c5b12a
Rahul Bhargava <rahulb@media.mit.edu>
2013-03-22 15:06:19 -0400

    check if image exists before creating in scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,23 +27,24 @@ class Scraper
 		media_list.each do |media|
 			(start_date..end_date).map do |date|
 
-				kiosko_url = Scraper::KIOSKO_BASE_URL + date.to_formatted_s(:kiosko_file_datestamp) + "/#{media.country_code}/#{media.name}.750.jpg"
-				puts kiosko_url
-				#TODO check if it exists first
-				img = Image.new
-				img.publication_date = date
-				img.media_id = media.id
-				img.image_name = media.name + "-" + img.publication_date.to_formatted_s(:file_datestamp)
-				if Scraper.use_local_images
-					img.local_path = media.local_image_dir + "/" + img.image_name
-					File.open(img.local_path, "wb") { |f| f.write(open(kiosko_url).read) }
-					size_info = Magick::ImageList.new(local_path)[0]
-					img.size = "#{size_info.columns}x#{size_info.rows}"
-				else
-					img.local_path = kiosko_url
-					img.size = '750x951' #!!this value of pixels is 'hard coded' so it gives wrong values for long newspapers
+				img = Image.find_by_media_id_and_publication_date(media.id, date)
+				if img==nil	# ensure only one entry per front page in the images table
+					kiosko_url = Scraper::KIOSKO_BASE_URL + date.to_formatted_s(:kiosko_file_datestamp) + "/#{media.country_code}/#{media.name}.750.jpg"
+					img = Image.new
+					img.publication_date = date
+					img.media_id = media.id
+					img.image_name = media.name + "-" + img.publication_date.to_formatted_s(:file_datestamp)
+					if Scraper.use_local_images
+						img.local_path = media.local_image_dir + "/" + img.image_name
+						File.open(img.local_path, "wb") { |f| f.write(open(kiosko_url).read) }
+						size_info = Magick::ImageList.new(local_path)[0]
+						img.size = "#{size_info.columns}x#{size_info.rows}"
+					else
+						img.local_path = kiosko_url
+						img.size = '750x951' #!!this value of pixels is 'hard coded' so it gives wrong values for long newspapers
+					end
+					img.save
 				end
-				img.save
 				images << img
 
 			end
