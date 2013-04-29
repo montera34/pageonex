f8e49e1cda3ea72a73eca405a8818e561c1f0af7
Edward L Platt <ed@elplatt.com>
2013-04-29 18:23:23 -0400

    Allows users with admin == true to edit and delte all threads for #130.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328193053) do
+ActiveRecord::Schema.define(:version => 20130429213553) do
 
   create_table "areas", :force => true do |t|
     t.integer  "x1"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20130328193053) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
