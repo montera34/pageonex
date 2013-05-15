4425094b6d0dddde1732bd62c905c01bc8e6aa1f
Rahul Bhargava <rahulb@media.mit.edu>
2013-05-15 11:15:01 -0400

    adds config for production email sending (#138)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,10 @@ Pageonex::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'pageonex.com' }
 
   # Enable threaded mode
   # config.threadsafe!
