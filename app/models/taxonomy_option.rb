162757fca8c0e2c6a28a85137fd404dc4ab5e538
Juanjo Bazán <jjbazan@gmail.com>
2018-07-11 12:11:05 +0200

    destroy taxonomy classifications when option is deleted

--- app/models/taxonomy_option.rb
+++ app/models/taxonomy_option.rb
@@ -1,5 +1,6 @@
 class TaxonomyOption < ActiveRecord::Base
   belongs_to :taxonomy
+  has_many :taxonomy_classifications, dependent: :destroy
 
   validates :value, presence: true, uniqueness: { scope: :taxonomy_id }
   validates :taxonomy_id, presence: true
