b35748308f4dc362192d024265fbb13bf23763c0
Rafael Porres Molina <rafa@sourced.tech>
2018-12-10 16:51:36 +0100

    Two docker-compose environments
    * One is meant to run the application from the pageonex images in
    Docker Hub
    * The other is meant to do development
    
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>

--- docker-compose-files/wrap-start-app.sh
+++ docker-compose-files/wrap-start-app.sh
@@ -8,14 +8,16 @@ password="$2"
 shift; shift
 cmd="$@"
 
+exec 2>&1
+
 rm -f /workspace/tmp/pids/server.pid
 
 port=${DATABASE_PORT:-3306}
 
 until mysql -u root --port $port --password=$password -h $host -e "SELECT 1"; do
-  >&2 echo "Mysql is unavailable - sleeping"
+   echo "Mysql is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Mysql is up - executing command"
-exec $cmd
+echo "Mysql is up - executing command"
+exec bash -c "$cmd"
